b4522d95ff838dcf6aee1dceb2b90ee56616ebb2
Wouter Van Schandevijl <woutervs@hotmail.com>
2022-10-05 19:52:36 +0200

    simple Heimdall config for testing purposes

---
+++ config/heimdall/www/SupportedApps/Bazarr/livestats.blade.php
@@ -0,0 +1,10 @@
+<ul class="livestats">
+    <li>
+        <span class="title">Movies</span>
+        <strong>{!! $movies !!}</strong>
+    </li>
+    <li>
+        <span class="title">Episodes</span>
+        <strong>{!! $series !!}</strong>
+    </li>
+</ul>
