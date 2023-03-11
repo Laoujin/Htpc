09259215232a65503dce6c2acf496fd79c61d2bb
Wouter Van Schandevijl <woutervs@hotmail.com>
2023-03-11 17:55:30 +0100

    finalization branch (watchtower issue, docker img saveing/loading...)

--- images/images-load.ps1
+++ images/images-load.ps1
@@ -8,6 +8,4 @@ docker load -i jellyfin.tar
 docker load -i ombi.tar
 docker load -i youtube-dl
 docker load -i readarr.tar
-docker load -i portainer-ce
-docker load -i watchtower.tar
-docker load -i duplicati.tar
+# docker load -i watchtower.tar
